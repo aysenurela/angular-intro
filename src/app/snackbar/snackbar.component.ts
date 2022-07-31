fac82117a1fa8e24c66bad0cd2c96da53e40427d
Ayse Ela <aysenurela@gmail.com>
2022-07-30 22:44:01 -0400

    snackbar demo

---
+++ src/app/snackbar/snackbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+/**
+ * @title Basic snack-bar
+ */
+@Component({
+  selector: 'app-snackbar',
+  templateUrl: 'snackbar.component.html'
+})
+export class SnackBarComponent {
+  constructor(private _snackBar: MatSnackBar) {}
+
+  openSnackBar() {
+    this._snackBar.open("snackbar", "X", {
+      duration: 1000
+    });
+  }
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
