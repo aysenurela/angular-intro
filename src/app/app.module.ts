4cc656b9527cde858a4d4d53447d599a01bcc886
Ayse Ela <aysenurela@gmail.com>
2022-07-30 22:10:07 -0400

    add material snackbar module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SnackBarComponent } from "./snackbar/snackbar.component"
 
 //@sth is "DECORATOR" (typescript feature)
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    SnackBarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatSnackBarModule
   ],
   providers: [],
-  bootstrap: [AppComponent, TodosComponent]
+  bootstrap: [AppComponent, TodosComponent, SnackBarComponent]
 })
 export class AppModule { } // no content
